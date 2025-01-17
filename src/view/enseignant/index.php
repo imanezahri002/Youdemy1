a16b8a45cba97f4332384f8dd065b3a166df6f83
imanezahri002 <imanezh5@gmail.com>
2025-01-17 01:26:33 +0100

    autorisation

--- src/view/enseignant/index.php
+++ src/view/enseignant/index.php
@@ -1,8 +1,16 @@
 <?php
+
+session_start();
+if(!isset($_SESSION["email"])){
+    header("location: ../connexion.php");
+};
+if ($_SESSION["role"] != 'teacher') {
+    header("location: ../connexion.php");
+};
 include '../layouts/header.php';
 include '../layouts/sidebar.php';
-?>
 
+?>
 
 
 
